c549a47b45995484220bcd41c52021996b2cb0c9
Sriiviidya <sridu17@gmail.com>
2019-02-04 00:59:54 -0500

    index_new

---
+++ scripts/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $(function () {
+       $(document).scroll(function () {
+         var $nav = $(".fixed-top");
+         $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+       });
+     });
+   })
