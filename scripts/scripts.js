cdb19dc132a26598818c80a476907c3760daecf6
Sriiviidya <sridu17@gmail.com>
2019-02-09 02:14:17 -0500

    glyphicons-added

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,13 +1,14 @@
 $(document).ready(function(){
-    $(function () {
-       $(document).scroll(function () {
-         var $nav = $(".fixed-top");
-         $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
-       });
+  $(function () {
+     $(document).scroll(function () {
+       var $nav = $(".fixed-top");
+       $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
      });
-   })
+   });
+ })
 
-   function scollDown() {
-    var elmnt = document.getElementById("work");
-    elmnt.scrollIntoView();
-  }
+ function scollDown() {
+  var elmnt = document.getElementById("work");
+  elmnt.scrollIntoView();
+}
+
